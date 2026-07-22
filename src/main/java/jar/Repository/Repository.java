322d1b7647e8a0d89f1aa870b810f5719bc5f70c
valentinopipitello-lab <valentinopipitello@gmail.com>
2026-07-22 10:19:04 -0300

    Delete src/main/java/jar/Repository/Repository.java

--- src/main/java/jar/Repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package jar.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import jar.entidades.Arma;
-public interface Repository extends JpaRepository<Arma, Long> {
-}
-
